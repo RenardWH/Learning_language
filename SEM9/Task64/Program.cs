1a2ced22a80f215b7fb8dde186f90141fb8e7ed0
Renard_WH <rybochkin116@gmail.com>
2022-09-22 00:14:52 +0400

    sem9 commit

---
+++ SEM9/Task64/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+
+Console.WriteLine("Введите число N");
+int n = Convert.ToInt32(Console.ReadLine());
+
+void PrintNumbers(int num)
+{
+    if (num == 0)
+    {
+        return;
+    }
+    Console.Write(num + " ");
+    PrintNumbers(num -1);
+}
+
+PrintNumbers(n);
+
---
+++ SEM9/Task66/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных 
+//элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.WriteLine("Введите число N");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число M");
+int m = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+
+
+void SumOfNumbers(int upperLimit, int lowLimit)
+{
+    
+    if (upperLimit == lowLimit-1)
+    {
+        return;
+    }
+    sum = sum + upperLimit;
+    SumOfNumbers(upperLimit -1, lowLimit);
+}
+if (n>m)
+{
+    SumOfNumbers(n, m);
+}
+else
+{
+    SumOfNumbers(m, n);
+}
+
+Console.WriteLine(sum);
